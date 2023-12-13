b4b123aa71bec25adcea496cc60433893dcab1f3
e-mbrown <ebsonari@gmail.com>
2023-12-13 16:15:41 -0500

    First commit WIP

---
+++ graph/character.resolvers.go
@@ -0,0 +1,8 @@
+package graph
+
+import "github.com/rollwod/graph/model"
+
+func (r *mutationResolver) CreateCharacter(ctx context.Context, input
+model.NewCharacter) (*model.Character, error) {
+	panic("not implemented")
+}
